1083d63e444d488f1aeaa25e0b8f872317c52b52
tomoki <benzousan7@gmail.com>
2023-02-27 10:12:56 +0900

    バリデーションに文字制限を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   has_many :task_categories, dependent: :destroy
   has_many :task_categories_info,through: :task_categories, source: :category
   scope :recent_order, -> {order(created_at: :desc)}
-  validates :name, presence: true
-  validates :content, presence: true
+  validates :name, presence: true, length: {maximum: 50}
+  validates :content, presence: true, length: {maximum: 255}
   validates :limit, presence: true
 end
